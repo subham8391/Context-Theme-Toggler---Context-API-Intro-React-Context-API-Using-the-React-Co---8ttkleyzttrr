3680ce30fe3f1e9a9ce9c084121f2a718849e39d
Subham Das <104473763+subham8391@users.noreply.github.com>
2023-09-11 22:03:57 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -1,39 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
-import { ThemeContext } from "./ThemeProvider";
+import React, { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from './ThemeProvider';
+
 
 const LocalThemedBox = () => {
-  const { theme } = useContext(ThemeContext);
-  const [localTheme, setLocalTheme] = useState(theme);
+    const [localTheme, setLocalTheme] = useState('dark');
+    const { theme, toggleTheme } = useContext(ThemeContext);
 
-  useEffect(() => {
-    setLocalTheme(theme);
-  }, [theme]);
+    useEffect(() => {
+        setLocalTheme(theme);
+    }, [theme]);
 
-  function changeLocalTheme() {
-    if (localTheme === "light") {
-      setLocalTheme("dark");
-    } else {
-      setLocalTheme("light");
-    }
-  }
 
-  return (
-    <div
-      style={{ width: "200px", height: "200px", border: "2px solid green" }}
-      id="local-themed-box"
-      className={`bg-${localTheme}`}
-    >
-      <p id="local-themed-text-container" className={`txt-${localTheme}`}>
-        Hey We did it!!
-      </p>
-      <button
-        id="local-theme-toggler"
-        onClick={changeLocalTheme}
-      >{`Toggle local theme to ${
-        localTheme == "light" ? "dark" : "light"
-      }`}</button>
-    </div>
-  );
-};
+    return (
+        <div style={{ width: '200px', height: '200px', border: '2px solid green' }} id="local-themed-box" className={`bg-${localTheme}`}>
+            {/* Write code below this line */}
+            <p id='local-themed-text-container' className={`txt-${localTheme}`}></p>
+            <button onClick={() => setLocalTheme(prev => prev === 'dark' ? 'light' : 'dark')} id="local-theme-toggler" className={`btn btn-${localTheme}`} >Toggle local theme to {localTheme === 'dark' ? 'light' : 'dark'}</button>
+        </div>
+    )
+}
 
 export { LocalThemedBox };
