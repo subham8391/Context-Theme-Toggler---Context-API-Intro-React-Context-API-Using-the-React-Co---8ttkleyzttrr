72696f0ba183584e89ced04e8cbad600c75dadd7
Subham Das <104473763+subham8391@users.noreply.github.com>
2023-09-11 22:14:11 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,19 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { LocalThemedBox } from './LocalThemedBox';
-import { ThemeContext } from './ThemeProvider';
+import React, { useContext } from "react";
+import { LocalThemedBox } from "./LocalThemedBox";
+import { ThemeContext } from "./ThemeProvider";
 
 const Page = () => {
-    const { theme, toggleTheme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div className={`container bg-${theme}`} id="themed-page">
+      <p id="themed-text-container" className={`txt-${theme}`}>
+        lorem ipsum dolor iterit n stuff
+      </p>
+      <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">
+        Themed Button
+      </button>
+      <LocalThemedBox />
+    </div>
+  );
+};
 
-    return (
-        <div className={`container bg-${theme}`} id="themed-page">
-            <p id="themed-text-container" className={`txt-${theme}`}>
-                lorem ipsum dolor iterit n stuff
-            </p>
-            <button onClick={toggleTheme} className={`btn btn-${theme} txt-${theme}`} id="themed-button">Themed Button</button>
-            <LocalThemedBox />
-        </div>
-    )
-}
-
-export { Page }
+export { Page };
